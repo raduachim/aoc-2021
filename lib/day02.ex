a5669310f721110572a2f3f36ffd3aa5f34ac986
Radu Achim <5114595+raduachim@users.noreply.github.com>
2022-08-19 14:20:15 +0200

    feat: 🎸 day 2 part 1

---
+++ lib/day02.ex
@@ -0,0 +1,41 @@
+defmodule Aoc.Day02 do
+  defp parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      [direction, value] = line |> String.split(" ")
+      {String.to_existing_atom(direction), String.to_integer(value)}
+    end)
+  end
+
+  defp get_input do
+    Aoc.Input.get!(2, 2021)
+    |> parse_input()
+  end
+
+  defp navigate(input) do
+    Enum.reduce(input, %{horizontal: 0, depth: 0}, fn
+      {:forward, value}, %{horizontal: horizontal} = acc ->
+        %{acc | horizontal: horizontal + value}
+
+      {:up, value}, %{depth: depth} = acc ->
+        %{acc | depth: depth - value}
+
+      {:down, value}, %{depth: depth} = acc ->
+        %{acc | depth: depth + value}
+    end)
+  end
+
+  defp multiply(%{horizontal: horizontal, depth: depth}), do: horizontal * depth
+
+  def part1 do
+    get_input()
+    |> navigate()
+    |> multiply()
+  end
+
+  def part2 do
+    get_input()
+  end
+end
